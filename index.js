17e3c3064556f2f647f0d6e9deaa88dfe914ef4f
EliasBinde <34208547+EliasBinde@users.noreply.github.com>
2022-05-18 11:39:04 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.get("/auth", (req, res) => {
+  res.send(`recieved with params: ${req.query.code}`);
+});
+
+app.listen(3000, () => {
+  console.log("Example app listening on port 3000!");
+});
